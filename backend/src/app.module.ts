c59f88b08c4b0669f52cea7be2a4d04a855d9603
vuphuong1794 <phuongvu1794@gmail.com>
2024-07-06 13:58:32 +0700

    fix bug

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { ServerModule } from './server/server.module';
 
 @Module({
-  imports: [],
+  imports: [
+    //hien tai dang o folder src nen se cd ra thu muc backend
+    //de dung duoc nhung folder chung (luu tru anh va hien thi o giao dien nguoi dung )
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/',
+    }),
+    GraphQLModule.forRootAsync({
+      imports: [],
+      inject: [], //them cac dich vu
+      driver: ApolloDriver,
+      useFactory: async ()=>{
+        return {
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          sortSchema: true,
+          subscriptions: {},
+        }
+      }
+    }),
+    ServerModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-
+import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 async function bootstrap() {
   const logger = new Logger('Main (main.ts)');
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+    allowedHeaders: [
+      "Accept",
+      "Authorization",
+      "Content-Type",
+      "X-Requested-With",
+      "apollo-require-preflight"
+    ]
+  })
+  app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 1 }));
   await app.listen(3000);
   logger.log('Server running on port 3000');
 }
