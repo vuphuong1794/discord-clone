3782535b37ce3dce037047fa9f9e6a945387b220
vuphuong1794 <phuongvu1794@gmail.com>
2024-07-06 11:12:24 +0700

    add prisma migrations

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main (main.ts)');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  logger.log('Server running on port 3000');
 }
 bootstrap();
